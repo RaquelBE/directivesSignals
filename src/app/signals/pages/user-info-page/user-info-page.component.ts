da2215b6019272c0fa355173c232ce2ab02ad19a
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-25 16:30:11 +0200

    información del usuario

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrls: ['./user-info-page.component.css']
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit{
+  
+  private usersService = inject(UsersServiceService);
+  public userId = signal(1);
+  
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFount = signal(true);
 
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
+
+  loadUser (id: number) {
+    if ( id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+    //petición http
+    this.usersService.getUserById(id)
+    .subscribe( user => {
+      this.currentUser.set(user)
+    })
+  }
 }
