9ecc49812bc249f3cc4cbb8042685cffed2c6983
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-25 12:04:10 +0200

    cambiar el color del host

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,17 +1,29 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
 })
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red'; //color por defecto cuando se inicializa
+  //recibir el color hexadecimal
+  @Input() set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
     //console.log('constructor de la directiva')
-    console.log(el);
+    //console.log(el);
     this.htmlElement = el;
   }
   ngOnInit(): void {
-    console.log('Directica - NgOnInit');
+    //console.log('Directica - NgOnInit');
+    this.setStyle();
+  }
+
+  setStyle():void {
+    if (!this.htmlElement) return;
+    this.htmlElement!.nativeElement.style.color = this._color;
   }
 }
