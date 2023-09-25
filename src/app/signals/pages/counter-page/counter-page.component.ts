18065ee65abf018c0295f4f52cd98ff9d8ea92a9
Raquel Blazquez <raquel.blazquez@bosonit.com>
2023-09-25 15:58:36 +0200

    counter signal

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+
 
 @Component({
-  selector: 'app-counter-page',
+  //selector: 'app-counter-page',
   templateUrl: './counter-page.component.html',
-  styleUrls: ['./counter-page.component.css']
+  styleUrls: ['./counter-page.component.css'],
 })
 export class CounterPageComponent {
+  //creación señal
+  public counter = signal(10);
+  public squareCounter = computed( () => this.counter() * this.counter());
 
+  increaseBy(value: number) {
+    //this.counter.set(this.counter() + value);
+    this.counter.update(current => current + value);
+  }
 }
